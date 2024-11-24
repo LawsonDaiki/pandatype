9c4bd4b438b94200f8ba634363e8cf1a057115d2
Lawson Daiki Hamasaki <lhamasaki@verzo.com.br>
2024-11-24 19:29:50 -0300

    Summary of the changes in pandatype.py : - add reset game feature - add end game message feature - add start message of how to reset game

--- pandatype.py
+++ pandatype.py
@@ -17,35 +17,56 @@ def main():
             self.stdscr = stdscr
             self.text = text
             self.user_text = ''
+
+        def current_idx(self):
+            return len(self.user_text)
         
         def print_text(self):
             for text_idx, letter_text in enumerate(self.text):
-                if text_idx == len(self.user_text):
+                if text_idx == self.current_idx(): # print the white characters
                     self.stdscr.addstr(f"{self.text[text_idx:]}")
                     break
-                elif letter_text == self.user_text[text_idx]:
+                elif letter_text == self.user_text[text_idx]: # print the green characters
                     self.stdscr.addstr(f"{letter_text}", curses.color_pair(2))
-                else:
+                else: # print the red characters
                     self.stdscr.addstr(f"{letter_text}", curses.color_pair(1))
             self.stdscr.addstr("\n")
             return
         
         def on_key_press(self, key):
-            if key == 127:
+            if key == 127: # on key 'Delete' erase the last input
                 self.user_text = self.user_text[:-1]
+            elif key == 9: # on key 'Tab' reset game
+                self.user_text = ''
             else:
                 self.user_text += chr(key)
+
             self.print_text()
+
+            if self.has_ended():
+                self.stdscr.addstr("wpm: <>\n")
+                self.stdscr.addstr("acc: <>\n")
+                self.stdscr.addstr("time: <>\n")
+                self.stdscr.addstr("test type: <>\n")
+        
+        def has_ended(self):
+            if self.current_idx() >= len(self.text):
+                return True
+            return False
+        
+        def reset(self):
+            self.user_text = ''
 
     def key_listener(stdscr):
         stdscr.nodelay(True)  # Make getch() non-blocking
 
-        def print_quit_message():
+        def print_start_message():
             stdscr.clear()
             stdscr.addstr("(Press 'ESC' to quit)...\n")
+            stdscr.addstr("(Press 'Tab' to reset)...\n")
             return
 
-        print_quit_message()
+        print_start_message()
 
         type_text = TypeText(stdscr, text)
         type_text.print_text()
@@ -53,18 +74,16 @@ def main():
         while True:
             key = stdscr.getch()
             if key != -1:  # If a key is pressed
-                print_quit_message()
-                type_text.on_key_press(key)
-                stdscr.addstr(f"Key pressed: {key} ('{chr(key)}')\n")
-                stdscr.refresh()
                 if key == 27:  # Exit on 'ESC'
                     break
+                
+                print_start_message()
+                type_text.on_key_press(key)
+
+                stdscr.addstr(f"\nKey pressed: {key} ('{chr(key)}')\n")
+                stdscr.refresh()
 
     curses.wrapper(key_listener) # Run the curses application
 
 if __name__ == "__main__":
-    # Entry point of the script
-    print("Script is being run directly.")
     main()
-else:
-    print("Script is being imported as a module.")
