5fae43047f8bb54554373125cc1cea705542420b
Lawson Daiki Hamasaki <lhamasaki@verzo.com.br>
2024-11-24 07:23:28 -0300

    refresh screen every key stroke

--- pandatype.py
+++ pandatype.py
@@ -1,5 +1,10 @@
 import curses
 
+def print_quit_message(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Press any key (Press 'ESC' to quit)...\n")
+    return
+
 def main():
     """
     Main function to execute the core logic of the script.
@@ -7,12 +12,12 @@ def main():
 
     def key_listener(stdscr):
         stdscr.nodelay(True)  # Make getch non-blocking
-        stdscr.clear()
-        stdscr.addstr("Press any key (Press 'ESC' to quit)...\n")
+        print_quit_message(stdscr)
         
         while True:
             key = stdscr.getch()
             if key != -1:  # If a key is pressed
+                print_quit_message(stdscr)
                 stdscr.addstr(f"Key pressed: {key} ('{chr(key)}')\n")
                 stdscr.refresh()
                 if key == 27:  # Exit on 'q'
