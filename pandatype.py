561a79340d1211c35c152501409353f74185cf49
Lawson Daiki Hamasaki <lhamasaki@verzo.com.br>
2024-11-25 17:35:32 -0300

    Summary of the changes in pandatype.py : - add stopwatch to show run time to finish game - add code for better structured header and footer message

--- pandatype.py
+++ pandatype.py
@@ -1,4 +1,5 @@
 import curses
+import time
 
 def main():
     """
@@ -15,12 +16,37 @@ def main():
     class TypeText:
         def __init__(self, stdscr, text):
             self.stdscr = stdscr
+            self.start_time = None
+            self.run_time = None
             self.text = text
             self.user_text = ''
 
         def current_idx(self):
             return len(self.user_text)
         
+        def start_stopwatch(self):
+            if self.current_idx() == 0:
+                self.start_time = time.time()
+
+        def elapsed_time(self):
+            if self.has_ended():
+                if not self.run_time:
+                    self.run_time = time.time() - self.start_time
+                return self.run_time
+            return time.time() - self.start_time
+        
+        def print_header_message(self):
+            self.stdscr.clear()
+            self.stdscr.addstr("(Press 'ESC' to quit)...\n")
+            self.stdscr.addstr("(Press 'Tab' to reset)...\n\n")
+        
+        def print_footer_message(self):
+            self.stdscr.addstr("\n")
+            self.stdscr.addstr(f"time: {self.elapsed_time():.2f} seconds\n")
+            self.stdscr.addstr("wpm: <>\n")
+            self.stdscr.addstr("acc: <>\n")
+            self.stdscr.addstr("test type: <>\n")
+
         def print_text(self):
             for text_idx, letter_text in enumerate(self.text):
                 if text_idx == self.current_idx(): # print the white characters
@@ -31,44 +57,39 @@ def main():
                 else: # print the red characters
                     self.stdscr.addstr(f"{letter_text}", curses.color_pair(1))
             self.stdscr.addstr("\n")
-            return
+        
+        def has_ended(self):
+            return self.current_idx() >= len(self.text)
+        
+        def reset(self):
+            self.user_text = ''
+            self.start_time = time.time() # It is like this (and not None) because print_footer_message() will need self.start_time to call self.elapsed_time() to print 0.00 seconds
+            self.run_time = None
         
         def on_key_press(self, key):
             if key == 127: # on key 'Delete' erase the last input
                 self.user_text = self.user_text[:-1]
             elif key == 9: # on key 'Tab' reset game
-                self.user_text = ''
+                self.reset()
             else:
+                self.start_stopwatch()
                 self.user_text += chr(key)
-
+            
+            self.print_header_message()
             self.print_text()
+            self.print_footer_message()
+
+            self.stdscr.addstr(f"\nKey pressed: {key} ('{chr(key)}')\n")
 
             if self.has_ended():
-                self.stdscr.addstr("wpm: <>\n")
-                self.stdscr.addstr("acc: <>\n")
-                self.stdscr.addstr("time: <>\n")
-                self.stdscr.addstr("test type: <>\n")
-        
-        def has_ended(self):
-            if self.current_idx() >= len(self.text):
-                return True
-            return False
-        
-        def reset(self):
-            self.user_text = ''
+                self.stdscr.addstr("\nGAME OVER\n", curses.color_pair(1))
 
     def key_listener(stdscr):
         stdscr.nodelay(True)  # Make getch() non-blocking
 
-        def print_start_message():
-            stdscr.clear()
-            stdscr.addstr("(Press 'ESC' to quit)...\n")
-            stdscr.addstr("(Press 'Tab' to reset)...\n")
-            return
-
-        print_start_message()
 
         type_text = TypeText(stdscr, text)
+        type_text.print_header_message()
         type_text.print_text()
         
         while True:
@@ -77,10 +98,8 @@ def main():
                 if key == 27:  # Exit on 'ESC'
                     break
                 
-                print_start_message()
                 type_text.on_key_press(key)
 
-                stdscr.addstr(f"\nKey pressed: {key} ('{chr(key)}')\n")
                 stdscr.refresh()
 
     curses.wrapper(key_listener) # Run the curses application
